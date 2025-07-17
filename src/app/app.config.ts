57d583f45e1fbc197c860562752e6e8096825160
Marco Andres Silva <marco.silvaponce10@gmail.com>
2025-07-16 21:13:03 -0400

    feat: creacion del componente principal y sus metodos e importaciones

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 
 import { routes } from './app.routes';
 
@@ -10,6 +18,15 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
     provideHttpClient(),
-    provideAnimations()
+    provideAnimations(),
+    importProvidersFrom(
+      ReactiveFormsModule,
+      MatFormFieldModule,
+      MatInputModule,
+      MatButtonModule,
+      MatIconModule,
+      MatCardModule,
+      MatProgressSpinnerModule
+    )
   ]
 };
--- src/app/components/weather-search/weather-search.component.ts
+++ src/app/components/weather-search/weather-search.component.ts
@@ -1,11 +1,71 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { finalize } from 'rxjs/operators';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+// Importación del Servicio
+import { WeatherService } from '../../services/weather.service';
+
 
 @Component({
   selector: 'app-weather-search',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatProgressSpinnerModule
+  ],
   templateUrl: './weather-search.component.html',
   styleUrl: './weather-search.component.scss'
 })
 export class WeatherSearchComponent {
+
+  cityControl = new FormControl('', [Validators.required, Validators.minLength(2)]);
+  weatherData: any = null;
+  isLoading = false;
+  errorMessage:string | null = null;
+
+  constructor(private weatherService: WeatherService) { }
+
+  searchWeather(): void {
+    if(this.cityControl.invalid)
+      return
+    
+
+    this.isLoading = true;
+    this.weatherData = null;
+    this.errorMessage = null;
+
+    const city = this.cityControl.value!;
+
+    this.weatherService.getWeatherByCity(city).pipe(
+      finalize(() => {
+        this.isLoading = false;
+      })
+    ).subscribe({
+      next: (data) => {
+        this.weatherData = data;
+      },
+      error: (error) => {
+        this.errorMessage = error.message;
+      }
+    });
+  }
+
+  getWeatherIconUrl(iconCode: string): string {
+    return `http://openweathermap.org/img/wn/${iconCode}@4x.png`;
+  }
+
 
 }
